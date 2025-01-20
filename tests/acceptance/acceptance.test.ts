cd516a69e6f9fbdc64a8a9b430be63f307e93f7f
matank78 <mayank.yadav@3tsoftwarelabs.com>
2025-01-20 18:17:26 +0100

    add placeholder test in system test

---
+++ tests/acceptance/acceptance.test.ts
@@ -0,0 +1,5 @@
+import { expect, test } from "vitest";
+
+test("Acceptance test", () => {
+    expect(1 + 1).toEqual(2);
+});
---
+++ tests/external-contract/external-contract.test.ts
@@ -0,0 +1,5 @@
+import { expect, test } from "vitest";
+
+test("External contract test", () => {
+    expect(1 + 1).toEqual(2);
+});
