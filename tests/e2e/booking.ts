cdbfc7c4d76ca93d1305685fe3cc35db0ab82d4d
Antonio Neves <antonio.neves@3tsoftwarelabs.com>
2025-01-27 14:32:22 +0100

    set desk booked by another employer

--- tests/e2e/booking.ts
+++ tests/e2e/booking.ts
@@ -1,22 +1,24 @@
-import {
-  When,
-  Then,
-  Given,
-  Before,
-} from "@badeball/cypress-cucumber-preprocessor";
+import { When, Then, Given } from "@badeball/cypress-cucumber-preprocessor";
 import dayjs from "dayjs";
 
 const bookDesk = async (deskNbr: number) => {
-  const res: any = await fetch(
-    Cypress.env("CYPRESS_BE_HOST") +
-      "/api/booking/?email=" +
-      Cypress.env("CYPRESS_TEST_USER_1_EMAIL"),
+  const res = await fetch(
+    Cypress.env("BE_HOST") +
+      "/api/booking?email=" +
+      Cypress.env("TEST_USER_1_EMAIL"),
     {
       method: "POST",
-      headers: { Authorization: Cypress.env("CYPRESS_TEST_USER_1_TOKEN") },
-      body: `{"deskId": ${deskNbr}, "date": "${dayjs().format("YYYY-MM-DD")}"}`,
+      headers: {
+        Authorization: Cypress.env("TEST_USER_1_TOKEN"),
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        deskId: deskNbr,
+        date: dayjs().format("YYYY-MM-DD"),
+      }),
     },
   );
+  console.error("Response: " + res);
 };
 
 Given("the employee is logged in", () => {
@@ -30,6 +32,9 @@ Given("the employee is on the desk booking page", () => {
 
 Given("desk {int} is already booked by another employee", (deskNbr: number) => {
   bookDesk(deskNbr);
+  cy.get(".desk")
+    .contains("Flex " + deskNbr)
+    .should("not.have.class", "available");
 });
 
 When("the employee selects desk {int}", (deskNbr) => {
@@ -39,7 +44,7 @@ When("the employee selects desk {int}", (deskNbr) => {
 });
 
 Then("desk {int} is not selected", (deskNbr: number) => {
-  const desk = cy.get(".desk").contains("Flex " + deskNbr);
-  desk.should("not.have.class", "available");
-  desk.should("not.have.class", "selected");
+  cy.get(".desk")
+    .contains("Flex " + deskNbr)
+    .should("not.have.class", "selected");
 });
