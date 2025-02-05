2d37dc0c0b551fe94059a21fbeea6c4998bb527e
Antonio Neves <antonio.neves@3tsoftwarelabs.com>
2025-02-05 11:15:32 +0100

    remove console logs spy

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -3,11 +3,4 @@ export const currentDate = Date.UTC(2099, 8, 15);
 
 beforeEach(() => {
   cy.clock(currentDate, ["Date"]);
-
-  cy.on("window:load", (win) => {
-    cy.spy(win.console, "log").as("consoleLog");
-    cy.spy(win.console, "error").as("consoleError");
-    cy.spy(win.console, "warn").as("consoleWarn");
-    cy.spy(win.console, "info").as("consoleInfo");
-  });
 });
